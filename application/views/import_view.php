00fa96130690cd50ad8e8e61a8cdb834232d6d0d
konradtomaszewski <konrad.tomaszewski85@gmail.com>
2017-12-14 15:27:10 +0100

    add div fixed

--- application/views/import_view.php
+++ application/views/import_view.php
@@ -41,43 +41,43 @@ $(document).ready(function() {
 
             <div class="mdl-grid mdl-cell mdl-cell--3-col-desktop mdl-cell--12-col-tablet mdl-cell--4-col-phone mdl-cell--top">
 			
-                <!-- Robot card-->
-				<div class="mdl-cell mdl-cell--12-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
-                    <div class="mdl-card mdl-shadow--2dp excel_to_db">
-                        <div class="mdl-card__title mdl-card--expand">
-                            <h2 class="mdl-card__title-text">Import danych</h2>
-                        </div>
-                        <div class="mdl-card__supporting-text">
-                            <div>
-                               Naciśnięcie przycisku spowoduje uruchomienie procesu odpwoedzialnego za import danych z katalogu files/xml do bazy danych
+                <div style="position:fixed">
+                    <div class="mdl-cell mdl-cell--12-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
+                        <div class="mdl-card mdl-shadow--2dp excel_to_db">
+                            <div class="mdl-card__title mdl-card--expand">
+                                <h2 class="mdl-card__title-text">Import danych</h2>
+                            </div>
+                            <div class="mdl-card__supporting-text">
+                                <div>
+                                Naciśnięcie przycisku spowoduje uruchomienie procesu odpwoedzialnego za import danych z katalogu files/xml do bazy danych
+                                </div>
+                            </div>
+                            <div class="mdl-card__actions">
+                                <button id="run_import" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored-blue" style="float:right">
+                                    Uruchom proces importu
+                                </button>
                             </div>
                         </div>
-                        <div class="mdl-card__actions">
-                            <button id="run_import" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored-blue" style="float:right">
-                                Uruchom proces importu
-                            </button>
+                    </div>
+                    <!-- ToDo_widget-->
+                    <div class="mdl-cell mdl-cell--12-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
+                        <div class="mdl-card mdl-shadow--2dp">
+                            <div class="mdl-card__title">
+                                <h2 class="mdl-card__title-text">Wyświetl dane archiwalne</h2>
+                            </div>
+                            <div class="mdl-card__supporting-text">
+                                <div>
+                                asd
+                                </div>
+                            </div>
+                            <div class="mdl-card__actions">
+                                <button id="show_archive" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored-blue" style="float:right">
+                                    Pokaż dane
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-				<!-- ToDo_widget-->
-                <div class="mdl-cell mdl-cell--12-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
-                    <div class="mdl-card mdl-shadow--2dp">
-                        <div class="mdl-card__title">
-                            <h2 class="mdl-card__title-text">Wyświetl dane archiwalne</h2>
-                        </div>
-                        <div class="mdl-card__supporting-text">
-                            <div>
-                            asd
-                            </div>
-                        </div>
-                        <div class="mdl-card__actions">
-                            <button id="show_archive" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored-blue" style="float:right">
-                                Pokaż dane
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
 
             </div>
         </div>
